d76c5b75eda8841e011186c0a7d6cc4e05c4634f
jteste11 <jteste11@stumail.northeaststate.edu>
2026-03-17 09:07:13 -0400

    Add line to display message on MAIN

--- lab.ps1
+++ lab.ps1
@@ -9,3 +9,4 @@ Write-Host "==============================="
 Write-Host $greeting
 Write-Host "Your name has $($name.Length) characters."
 Write-Host "==============================="
+Write-Host "This line was added on MAIN"
